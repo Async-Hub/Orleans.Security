46237ea339c1928bd1dc4d6b7ba7ac34edf0b375
Karen Tazayan <39263060+KarenTazayan@users.noreply.github.com>
2019-08-31 11:16:28 +0400

    Microsoft Orleans packages updated to the version v2.4.1. (#12)
    * Microsoft Orleans packages updated to the version v2.4.1.
    
    * Azure DevOps feed id was updated.
    
    * Added GitVersion into Azure DevOps pipeline.
    
    * The following "#arguments: '--logger:trx'" excluded from Run Tests task in Azure DevOps pipline.
    
    * Modified the order of "Version with GitVersion" task in Azure DevOps pipeline config file.
    
    * "<GenerateAssemblyInfo>false</GenerateAssemblyInfo>" added into Orleans.Security project.
    
    * "<GenerateAssemblyInfo>false</GenerateAssemblyInfo>" added into Orleans.Security.Client/Orleans.Security.Clustering project.
    
    * Some AssemblyVersion attributes added into scr projects.
    
    * Azure DevOps build pipline configuration updated. Task "Version with GitVersion" moved to the top.
    
    * Added command argument "arguments: '/p:Version=$(Build.BuildNumber)'" into task "NuGet pack Orleans.Security.Build.sln" in azure-pipelines.yml.
    
    * Changed version scheme for task "NuGet pack Orleans.Security.Build.sln" in azure-pipelines.yml.

--- src/Orleans.Security/AssemblyInfo.cs
+++ src/Orleans.Security/AssemblyInfo.cs
@@ -5,3 +5,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Orleans.Security.Clustering")]
 [assembly: InternalsVisibleTo("Orleans.Security.IntegrationTests.Functional")]
 [assembly: InternalsVisibleTo("Orleans.Security.IntegrationTests.TokenVerification")]
+
+[assembly: AssemblyVersion("2.4.1.0")]
+[assembly: AssemblyFileVersion("2.4.1.0")]
+[assembly: AssemblyInformationalVersion("2.4.1-upgrade-to-ms-orleans.7+Branch.feature/upgrade-to-ms-orleans-v2.4.1.Sha.28005336790aafa81b1e204a62018caf545e3a1c")]
