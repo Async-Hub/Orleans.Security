bfe147829dc11244d4ad9817ae032943a22c5e34
Karen Tazayan <39263060+KarenTazayan@users.noreply.github.com>
2019-12-26 18:48:49 +0400

    Migrate from .NET Core 3.0 to 3.1 (#23)
    * Microsoft Orleans packages updated to the version v2.4.1. (#12)
    
    * Microsoft Orleans packages updated to the version v2.4.1.
    
    * Azure DevOps feed id was updated.
    
    * Added GitVersion into Azure DevOps pipeline.
    
    * The following "#arguments: '--logger:trx'" excluded from Run Tests task in Azure DevOps pipline.
    
    * Modified the order of "Version with GitVersion" task in Azure DevOps pipeline config file.
    
    * "<GenerateAssemblyInfo>false</GenerateAssemblyInfo>" added into Orleans.Security project.
    
    * "<GenerateAssemblyInfo>false</GenerateAssemblyInfo>" added into Orleans.Security.Client/Orleans.Security.Clustering project.
    
    * Some AssemblyVersion attributes added into scr projects.
    
    * Azure DevOps build pipline configuration updated. Task "Version with GitVersion" moved to the top.
    
    * Added command argument "arguments: '/p:Version=$(Build.BuildNumber)'" into task "NuGet pack Orleans.Security.Build.sln" in azure-pipelines.yml.
    
    * Changed version scheme for task "NuGet pack Orleans.Security.Build.sln" in azure-pipelines.yml.
    
    * Added "AsyncHub.Orleans.Security" feed into pipline. (#14)
    
    * Completed: Update IdentityServer from version 2.4.x to version 2.5.x. (#16)
    
    * Task "Run Code Analysis" with SonarCloud added into Azure DevOps pipline.
    
    * Modified obsolete IdentityModel api in ConsoleClient. (#18)
    
    * New samples for IdentityServer 4 v 3.0, ASP.NET Core 3.0, MS Orleans 3.0 (#19)
    
    * Two new folders with projects created for samples version 2.2 and samples version 3.0.
    
    * All packages in samples version-3-0 (excepted IdentityModel package in Orleans.Security) updated.
    
    * IdentityModel DiscoveryDocument fully encapsulated into type IdS4DiscoveryDocumentProvider.
    
    * SiloHost1 in version-3-0 updated to MS Orleans 3.0.
    
    * Resolved IdentityModel v3 and IdentityModel v4 runtime binding issues for "Orleans.Security" project.
    
    * Task "GitVersion" updated to version 5 in Azure DevOps pipeline.
    
    * Project Orleans.Security.IntegrationTests.Functional updated to use .NET Core 3.0.
    
    * Added the second job into Azure DevOps pipeline.
    
    * Passed "GitVersion.NuGetVersion" as output environment variable in Azure DevOps pipeline.
    
    * Fixed issue for: "GitVersion.NuGetVersion" as output environment variable in Azure DevOps pipeline.
    
    * Changed "$GitVersion.NuGetVersion" to "$(GitVersion.NuGetVersion)" into "##vso[task.setvariable" in Azure DevOps pipeline.
    
    * Changed "$GitVersion.NuGetVersion" to "$env:GITVERSION_NUGETVERSION" into "##vso[task.setvariable" in Azure DevOps pipeline.
    
    * Added name for task "# Pass $(GitVersion.NuGetVersion) to the next job" in Azure DevOps pipeline.
    
    * Try to configure www.nuget.org for Azure DevOps pipeline.
    
    * Changed "nuGetServiceConnections" name in Azure DevOps pipeline.
    
    * Fixed connection issue to the nuget.org in Azure DevOps pipeline.
    
    * Task "Push NuGet packs to Azure Artifacts feed" modified to be optional in Azure DevOps pipeline.
    
    * Feed name replaced with Feed Id for task "Push NuGet packs to Azure Artifacts feed" in Azure DevOps pipeline.
    
    * Directories for samples renamed from "version" to "dotnetcore".
    
    * Completed: Migrate from ASP.NET Core 3.0 to 3.1 for all web-based sample projects.
    
    * Completed: Migrate from .NET Core 3.0 to 3.1 for all other types of projects.
    
    * Fixed Generic Host issue for "TestIdentityServer4Builder".

---
+++ samples/dotnetcore-3-1/IdentityServer/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer4
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // configure identity server with in-memory stores, keys, clients and resources
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(IdentityServerConfig.GetApiResources())
+                .AddInMemoryIdentityResources(IdentityServerConfig.GetIdentityResources())
+                .AddInMemoryClients(IdentityServerConfig.GetClients())
+                .AddTestUsers(IdentityServerConfig.GetUsers());
+
+            services.AddControllersWithViews(); ;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseIdentityServer();
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
--- test/Orleans.Security.IntegrationTests.TokenVerification/Configuration/TestIdentityServer4Builder.cs
+++ test/Orleans.Security.IntegrationTests.TokenVerification/Configuration/TestIdentityServer4Builder.cs
@@ -3,38 +3,46 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Orleans.Security.IntegrationTests.TokenVerification.Configuration
 {
     public static class TestIdentityServer4Builder
     {
-        public static TestServer BuildNew()
+        public static TestServer StartNew()
         {
-            return BuildTestServer();
-        }
-
-        private static TestServer BuildTestServer()
-        {
-            var builder = new WebHostBuilder()
-                .UseUrls("http://localhost:5001")
-                .ConfigureServices(services =>
+            var builder = new HostBuilder()
+                .ConfigureWebHost(webHost =>
                 {
-                    services.AddIdentityServer()
-                        .AddDeveloperSigningCredential()
-                        .AddInMemoryApiResources(IdentityServerConfig.GetApiResources())
-                        .AddInMemoryIdentityResources(IdentityServerConfig.GetIdentityResources())
-                        .AddInMemoryClients(IdentityServerConfig.GetClients())
-                        .AddTestUsers(IdentityServerConfig.GetUsers());
+                    webHost
+                        .UseUrls("http://localhost:5001")
+                        .UseTestServer()
+                        .ConfigureServices(services =>
+                        {
+                            services.AddIdentityServer()
+                                .AddDeveloperSigningCredential()
+                                .AddInMemoryApiResources(IdentityServerConfig.GetApiResources())
+                                .AddInMemoryIdentityResources(IdentityServerConfig.GetIdentityResources())
+                                .AddInMemoryClients(IdentityServerConfig.GetClients())
+                                .AddTestUsers(IdentityServerConfig.GetUsers());
 
-                    services.AddMvcCore();
-                })
-                .Configure(app =>
-                {
-                    app.UseIdentityServer();
-                    app.UseMvcWithDefaultRoute();
+                            services.AddControllers();
+                        })
+                        .Configure(app =>
+                        {
+                            app.UseIdentityServer();
+                            app.UseRouting();
+
+                            app.UseEndpoints(endpoints =>
+                            {
+                                endpoints.MapDefaultControllerRoute();
+                            });
+                        });
                 });
 
-            var identityServer4 = new TestServer(builder);
+            var host = builder.Start();
+
+            var identityServer4 = host.GetTestServer();
             identityServer4.CreateHandler();
             identityServer4.BaseAddress = new Uri("http://localhost:5001");
 
