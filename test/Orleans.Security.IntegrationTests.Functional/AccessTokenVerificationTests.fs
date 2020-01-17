2b1768a208419350dedfd892e15e8e37593db6e3
Karen Tazayan <karen.tazayan@outlook.com>
2020-01-17 11:55:31 +0400

    New "Access token verification with invalid scope should be failed" test added.

--- test/Orleans.Security.IntegrationTests.Functional/AccessTokenVerificationTests.fs
+++ test/Orleans.Security.IntegrationTests.Functional/AccessTokenVerificationTests.fs
@@ -1,20 +1,41 @@
 ﻿module AccessTokenVerificationTests
 
 open Xunit
+open Microsoft.IdentityModel.Tokens
 open Orleans.Security.AccessToken
 
 [<Theory>]
 [<InlineData("WebClient", "Secret", "Api1")>]
-let ``Access token verification with valid scope should be passed`` 
-    (clientId:string) (clientSecret:string) (scope:string) = async {
+let ``Access token verification with valid scope should be passed``
+    (clientId: string) (clientSecret: string) (scope: string) =
+    async {
         // Arrange
-        let! accessTokenResponse = IdentityServer4.requestClientCredentialsTokenAsync 
-                                    clientId clientSecret scope |> Async.AwaitTask
+        let! accessTokenResponse = IdentityServer4.requestClientCredentialsTokenAsync clientId clientSecret scope
+                                   |> Async.AwaitTask
 
         // Act
-        let claims = JwtSecurityTokenVerifier.Verify(accessTokenResponse.AccessToken, 
-                        scope, IdentityServer4.discoveryDocument)
-    
+        let claims =
+            JwtSecurityTokenVerifier.Verify(accessTokenResponse.AccessToken, scope, IdentityServer4.discoveryDocument)
+
         // Assert
-        Assert.True(claims <> null)
+        Assert.True(claims |> Seq.exists (fun c -> c.Type = "aud" && c.Value = scope))
+    }
+
+[<Theory>]
+[<InlineData("WebClient", "Secret", "Api2")>]
+let ``Access token verification with invalid scope should be failed``
+    (clientId: string) (clientSecret: string) (scope: string) =
+    async {
+        // Arrange
+        let! accessTokenResponse = IdentityServer4.requestClientCredentialsTokenAsync clientId clientSecret "Api1"
+                                   |> Async.AwaitTask
+
+        // Act
+        let verify =
+            fun () ->
+                JwtSecurityTokenVerifier.Verify
+                    (accessTokenResponse.AccessToken, scope, IdentityServer4.discoveryDocument) |> ignore
+
+        // Assert
+        Assert.Throws<SecurityTokenInvalidAudienceException>(verify) |> ignore
     }
