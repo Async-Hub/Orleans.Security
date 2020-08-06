eae8ec4ee4ab5c3b17bb9edb41ead88dbf7f68e8
Karen Tazayan <karen.tazayan@outlook.com>
2020-08-06 19:55:47 +0400

    Moved "Orleans.Security.Caching" from C# to "Orleans.Security.FShrap".

---
+++ src/Orleans.Security.FShrap/Caching/AccessTokenCache.fs
@@ -0,0 +1,33 @@
+﻿namespace Orleans.Security.Caching
+
+open System
+open Microsoft.Extensions.Caching.Memory
+open Microsoft.Extensions.Options
+
+type internal AccessTokenCache(memoryCacheOptions: IOptions<MemoryCacheOptions>) =
+    do
+        if memoryCacheOptions = null then raise(new ArgumentNullException("memoryCacheOptions"))
+    interface IAccessTokenCache with
+        member val Current : IMemoryCache = new MemoryCache(memoryCacheOptions) :> IMemoryCache
+
+//using System;
+//using Microsoft.Extensions.Caching.Memory;
+//using Microsoft.Extensions.Options;
+
+//namespace Orleans.Security.Caching
+//{
+//    internal class AccessTokenCache : IAccessTokenCache
+//    {
+//        internal AccessTokenCache(IOptions<MemoryCacheOptions> memoryCacheOptions)
+//        {
+//            if (memoryCacheOptions == null)
+//            {
+//                throw new ArgumentNullException(nameof(memoryCacheOptions));
+//            }
+
+//            Current = new MemoryCache(memoryCacheOptions);
+//        }
+
+//        public IMemoryCache Current { get; }
+//    }
+//}
---
+++ src/Orleans.Security.FShrap/IAccessTokenProvider.fs
@@ -0,0 +1,16 @@
+﻿namespace Orleans.Security
+
+open System.Threading.Tasks
+
+type IAccessTokenProvider =
+    abstract member RetrieveTokenAsync : unit -> Task<string>
+
+//using System.Threading.Tasks;
+
+//namespace Orleans.Security
+//{
+//    public interface IAccessTokenProvider
+//    {
+//        Task<string> RetrieveTokenAsync();
+//    }
+//}
