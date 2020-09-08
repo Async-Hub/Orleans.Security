1b1e71f941b2c450f121b06491cf94e1f2ff1390
Karen Tazayan <karen.tazayan@outlook.com>
2020-09-08 14:40:19 +0400

    Fixed: "FS0039: The type 'AssemblyVersion' is not defined.".

--- src/Orleans.Security.FShrap/AssemblyInfo.fs
+++ src/Orleans.Security.FShrap/AssemblyInfo.fs
@@ -1,7 +1,12 @@
 ﻿namespace Orleans.Security.FSharp
 
+open System.Reflection
 open System.Runtime.CompilerServices
 
 [<assembly: InternalsVisibleTo("Orleans.Security")>]
 [<assembly: InternalsVisibleTo("Orleans.Security.Tests")>]
+//TODO: Check why we need this.
+[<assembly: AssemblyVersion("2.4.1.0")>]
+[<assembly: AssemblyFileVersion("2.4.1.0")>]
+[<assembly: AssemblyInformationalVersion("2.4.1")>]
 do()
